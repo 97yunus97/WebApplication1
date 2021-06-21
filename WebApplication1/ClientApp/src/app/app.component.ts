f42c87bed0694eaa1c907840e3998ef8cb6352ae
Yunus BALLI <yunus.balli@veripark.com>
2021-06-21 18:11:44 +0300

    Proje Sonu

--- WebApplication1/ClientApp/src/app/app.component.ts
+++ WebApplication1/ClientApp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Action } from 'rxjs/scheduler/Action';
-import { Model } from './model';
+import { Model, ToDoItem } from './model';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +10,18 @@ import { Model } from './model';
 export class AppComponent {
 
   model = new Model
+  isDisplay = false;
 
   getItems() {
-    return this.model.items;
+    if (this.isDisplay) {
+      return this.model.items;
+    }
+    return this.model.items.filter(item => !item.action);
+  }
+
+  addItem(value) {
+    if (value != "") {
+      this.model.items.push(new ToDoItem(value,false))
+    }
   }
 }
